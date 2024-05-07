267f63fe14d6a24d72e64de92f25846200cf4f55
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-07 09:16:25 +0430

    using react routers

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,10 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime aperiam
+        eius repellat recusandae labore enim magnam voluptates veritatis debitis
+        quibusdam!recusandae labore enim magnam voluptates veritatis debitis
+        quibusdam!
+      </p>
+    </section>
+  );
 }
 
 export default About
