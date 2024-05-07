201b3be24faf92ac0ef5b1005248acece62c8d17
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-07 20:01:54 +0430

    Fetching data

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="cocktails/:id" element={<Home />} />
+          <Route path="cocktails/:id" element={<SingleCocktail />} />
           <Route path="*" element={<Error />} />
         </Routes>
       </Router>
