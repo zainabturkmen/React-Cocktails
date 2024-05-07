f8ea6e47cf06f903cfc86a31faa6ab3f51b671d9
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-07 10:27:50 +0430

    using react routers

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import Cocktail from './Cocktail'
-import Loading from './Loading'
-import { useGlobalContext } from '../context'
+import React from "react";
+import Cocktail from "./Cocktail";
+import Loading from "./Loading";
+import { useGlobalContext } from "../context";
 
 const CocktailList = () => {
   const { cocktails, loading } = useGlobalContext();
 
   if (loading) {
-    return <Loading />
+    return <Loading />;
   }
 
   if (cocktails.length < 1) {
-    return <h2 className='section-title'>no cocktails matched your search criteria</h2>
+    return (
+      <h2 className="section-title">
+        no cocktails matched your search criteria
+      </h2>
+    );
   }
-}
+};
 
-export default CocktailList
+export default CocktailList;
