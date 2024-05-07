678c57bdbca162499006cb7fecaa4edab5abb88b
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-07 10:16:19 +0430

    using react routers

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
+  const {setSearchTerm} = useGlobalContext()
   return (
     <div>
       <h2>search form component</h2>
