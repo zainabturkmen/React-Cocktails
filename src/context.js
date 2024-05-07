4691d9bf2c4d623bda134b31434618a1917ba496
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-07 10:05:11 +0430

    using react routers

--- src/context.js
+++ src/context.js
@@ -1,15 +1,22 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { useCallback } from 'react'
+import React, { useState, useContext, useEffect } from "react";
+import { useCallback } from "react";
 
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
-const AppContext = React.createContext()
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
+  const [loading, setLoading] = useState(true)
+  const [searchTerm, setSearchTerm] = useState("a")
+
+
+
+
+
+  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
