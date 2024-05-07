cd05f9e6f454625dc6a7153556ec6769ee88184f
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-07 12:32:19 +0430

    Fetching data

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Cocktail = () => {
+const Cocktail = ({image, name, id, info, glass}) => {
   return (
-    <div>
-      <h2>cocktail component</h2>
-    </div>
+   <article className='cocktails'></article>
   )
 }
 
