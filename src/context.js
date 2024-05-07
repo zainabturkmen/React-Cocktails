06eec69d70ba9066e43b05c507ecbc85b7a20add
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-07 10:10:36 +0430

    using react routers

--- src/context.js
+++ src/context.js
@@ -9,11 +9,14 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState("a")
   const [cocktails, setCocktails] = useState([])
 
-
-
-
-
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+  return <AppContext.Provider value={
+    {
+      loading, 
+      searchTerm,
+      cocktails,
+      setSearchTerm
+    }
+  }>{children}</AppContext.Provider>;
 };
 // make sure use
 export const useGlobalContext = () => {
