08e60c5b5ee6142f3656b9dc92fa8cdd7d9935be
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-07 12:45:21 +0430

    Fetching data

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -11,7 +11,7 @@ const Cocktail = ({image, name, id, info, glass}) => {
         <h3>{name}</h3>
         <h4>{glass}</h4>
         <p>{info}</p>
-        <Link to={`/cocktail/${id}`}></Link>
+        <Link to={`/cocktail/${id}`} className="btn btn-primary"></Link>
       </div>
     </article>
   );
